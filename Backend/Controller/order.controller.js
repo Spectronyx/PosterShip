5ab543a60c3aa1a34bf7edf261938d5f7d57d26b
Rajneesh Sharma <pt.rj578@gmail.com>
2026-01-21 12:50:20 +0530

    added gitignore

--- Backend/Controller/order.controller.js
+++ Backend/Controller/order.controller.js
@@ -1,5 +1,6 @@
 import expressAsyncHandler from "express-async-handler";
 import Order from "../Model/order.model.js";
+import mongoose from "mongoose";
 
 export const addOrderItems = expressAsyncHandler(async (req, res) => {
     const {
@@ -27,4 +28,30 @@ export const addOrderItems = expressAsyncHandler(async (req, res) => {
         const createdOrder = await order.save();
         res.status(201).json(createdOrder);
     }
+});
+
+
+export const getOrderById = expressAsyncHandler(async (req,res) => {
+
+    const order = await Order.findById(req.params.id).populate("user", "name email");
+
+    if(order){
+        res.json(order);
+    }else{
+        res.status(404);
+        throw new Error("cannot find the order");
+    }
+});
+
+// @desc    Get logged in user orders
+// @route   GET /api/order/myorders
+// @access  Private
+
+export const getMyOrder = expressAsyncHandler(async (req,res) => {
+    if(!req.user){
+        throw new Error("you are nto authorised fro this task");
+    }
+    const orders = await Order.find({user: req.user._id}).sort({createdAt: -1})
+
+    res.json(orders);
 });
--- Backend/Route/order.route.js
+++ Backend/Route/order.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems } from "../Controller/order.controller.js";
+import { addOrderItems, getMyOrder, getOrderById } from "../Controller/order.controller.js";
 import protect from "../Middleware/auth.middleware.js";
 
 // POST /api/order
 router.route("/").post(protect, addOrderItems);
+router.route("/mine").get(protect,getMyOrder);
+router.route("/:id").get(protect, getOrderById);
 
 export default router;
